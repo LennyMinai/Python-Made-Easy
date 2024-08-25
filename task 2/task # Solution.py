0e55356a44c2a3089cf05c9941fa2e9d577907aa
Lenny Minai <144992364+LennyMinai@users.noreply.github.com>
2024-08-25 23:54:00 +0300

    Day 2
    Basic Arithmetic Operations

---
+++ task 2/task # Solution.py
@@ -0,0 +1,23 @@
+#Addition of 10+6
+print(10+6)
+#Subtraction of 10-6
+print(6-3)
+#Division of 10 by 6
+print(10/6)
+#Multiplication of 10 by 6
+print(10*6)
+
+#approach 2 - Let's optimize the code even further to make it simpler to change the values in the code above
+
+#let 10 be "a" and 6 be "b"
+a = 10
+b = 6
+
+#Addition of 10+6
+print(a+b)
+#Subtraction of 10-6
+print(a-b)
+#Division of 10 by 6
+print(a/b)
+#Multiplication of 10 by 6
+print(a*b)
---
+++ task 2/task #2 Question.md
@@ -0,0 +1,10 @@
+TOPIC - BASIC ARITHMETIC OPERATIONS
+
+TASK 2
+
+Perform the following operations on 10 and 6 and print their outputs:
+
+1. Addition
+2. Subtraction
+3. Multiplication
+4. Division
